0725660d4192974ce5c25b8d9ed756d8d25ce930
GREEN780\konch_000 <hixi365@gmail.com>
2017-11-11 19:10:17 +0900

    ブロック配置 ランダムにフェードイン

---
+++ Assets/Scripts/test/TestAppearBlock.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// テスト
+// ブロック発生
+
+public class TestAppearBlock : MonoBehaviour {
+
+
+	public float timeMove = 1f; // 移動に掛かる時間
+
+	public Vector3 fromPos;		// 開始座標
+	public Vector3 fromRot;		// 開始回転
+
+	public Vector3 toPos;		// 終了座標
+	public Vector3 toRot;       // 終了回転
+
+	public float fromAlpha = 0f;	// 開始α値
+	public float toAlpha = 1f;		// 終了α値
+
+	private float time = 0f;    // 時間
+
+	private SpriteRenderer renderer;
+
+	private void Start()
+	{
+
+		BoxCollider2D c = GetComponent<BoxCollider2D>();
+		c.enabled = false;
+
+		transform.localPosition = fromPos;
+		transform.localRotation = Quaternion.Euler(fromRot);
+
+		renderer = GetComponent<SpriteRenderer>();
+		if (renderer) {
+			Color col = renderer.color;
+			col.a = fromAlpha;
+			renderer.color = col;
+		}
+
+	}
+
+	private void Update()
+	{
+
+		time += Time.deltaTime;
+
+		if(time < timeMove)
+		{
+
+			float a = time / timeMove;
+			Vector3 p = Vector3.Lerp(fromPos, toPos, a);
+			Quaternion q = Quaternion.Lerp(Quaternion.Euler(fromPos), Quaternion.Euler(toPos), a);
+
+			transform.localPosition = p;
+			transform.localRotation = q;
+
+			if (renderer)
+			{
+				Color col = renderer.color;
+				col.a = Mathf.Lerp(fromAlpha, toAlpha, a);
+				renderer.color = col;
+			}
+
+			return;
+
+		}
+
+		transform.localPosition = toPos;
+		transform.localRotation = Quaternion.Euler(toRot);
+
+		if (renderer)
+		{
+			Color col = renderer.color;
+			col.a = toAlpha;
+			renderer.color = col;
+		}
+
+		BoxCollider2D c = GetComponent<BoxCollider2D>();
+		if(c)
+			c.enabled = true;
+
+		Destroy(this);
+
+	}
+
+}
---
+++ Assets/Scripts/test/TestSetAppearBlocks.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// テスト
+// ブロック出現
+
+public class TestSetAppearBlocks : MonoBehaviour {
+
+
+	private void Awake()
+	{
+
+		TestAppearBlock[] objs = GetComponentsInChildren<TestAppearBlock>(true);
+
+		foreach (TestAppearBlock obj in objs)
+		{
+			obj.gameObject.SetActive(false);
+		}
+
+	}
+
+	private void Start()
+	{
+
+		RandomAppear();
+
+	}
+		
+	private void RandomAppear()
+	{
+
+		TestAppearBlock[] objs = GetComponentsInChildren<TestAppearBlock>(true);
+
+		foreach (TestAppearBlock obj in objs)
+		{
+
+			obj.gameObject.SetActive(true);
+
+			float a = Random.Range(0, 2 * Mathf.PI);
+			float rad = 1.0f;
+			float roll = Random.Range(-Mathf.PI, +Mathf.PI) * 3f;
+
+			Vector3 offsetPos = new Vector3(Mathf.Cos(a), Mathf.Sin(a), 0f) * rad;
+			Vector3 offsetRot = new Vector3(0f, 0f, roll);
+
+			obj.fromPos = obj.transform.localPosition - offsetPos;
+			obj.fromRot = obj.transform.localRotation.eulerAngles - offsetRot;
+
+			obj.toPos = obj.transform.localPosition;
+			obj.toRot = obj.transform.localRotation.eulerAngles;
+
+		}
+
+	}
+
+}
