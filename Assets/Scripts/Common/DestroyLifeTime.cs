66d6547c77584d77eb5a51a40693d5366882f0e6
GREEN780\konch_000 <hixi365@gmail.com>
2017-12-14 17:14:07 +0900

    スクリプトの書き直し (DestroyLifeTime.cs)

--- Assets/Scripts/Common/DestroyLifeTime.cs
+++ Assets/Scripts/Common/DestroyLifeTime.cs
@@ -1,20 +1,34 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-// 一定時間後にゲームオブジェクトを削除する
+// 汎用
+//  一定時間後にゲームオブジェクトを削除する
 
 public class DestroyLifeTime : MonoBehaviour {
 
-	public float lifeTime = 1f;
+	[SerializeField]
+	private float lifeTime = 1f;
 	private float timeTotal = 0f;
 	
+	public void SetLifeTime(float seconds)
+	{
+
+		lifeTime = seconds;
+
+	}
+
 	private void Update ()
 	{
 
 		timeTotal += Time.deltaTime;
+
 		if (timeTotal >= lifeTime)
+		{
 			Destroy(gameObject);
+		}
+
 	}
 
 }
