14a0a252622bd631345c1798316396874d80a13c
konch\konch <hixi365@gmail.com>
2018-01-05 11:19:37 +0900

    蛇の胴部分の生成

---
+++ Assets/Scripts/GameObject/SnakeManager.cs
@@ -0,0 +1,115 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// ゲームオブジェクト
+//  蛇 クラス
+
+public class SnakeManager : MonoBehaviour {
+
+	// prefab
+	[SerializeField]
+	private GameObject prefabHead;
+	[SerializeField]
+	private GameObject prefabBody;
+
+	// 生成したオブジェクト
+	private GameObject objHead;
+	private GameObject[] objBody;
+
+	// 生成パラメータ
+	[SerializeField]
+	private int lengthBody; // 生成時の胴体の数
+	[SerializeField]
+	private float intervalBodytoBody; // ボディ間のオフセット 
+
+	// head 制御
+	private Vector2 wposHead;
+	private float angleHead;
+
+	// body 制御
+	private Vector2[] wposBody;
+	private float[] angleBody;
+
+	
+
+	private void Start()
+	{
+
+		CreateSnake();
+
+	}
+
+	private void Update ()
+	{
+		
+	}
+
+
+	// 上向きの蛇を作り出す
+	private void CreateSnake()
+	{
+
+		// 胴体が無いものは生成しない
+		if (lengthBody <= 0)
+		{
+
+			Debug.Log("SnakeManager : lengthBody is 0");
+			return;
+
+		}
+
+		// prefabが未指定の場合は生成しない
+		if(prefabBody == null || prefabHead == null)
+		{
+
+			Debug.Log("SnakeManager : prefab is null");
+			return;
+
+		}
+
+		// 配列の確保
+		wposBody = new Vector2[lengthBody];
+		angleBody = new float[lengthBody];
+
+		// 頭の生成
+		Vector2 pos = transform.position;
+		float angle = transform.rotation.eulerAngles.z;
+
+		objHead = Instantiate(prefabHead, (Vector3)pos, Quaternion.Euler(0f, 0f, angle), transform);
+		GameObject objHeadObject = objHead.transform.GetChild(0).gameObject;
+
+		if(objHeadObject != null)
+		{
+			pos.y -= objHeadObject.transform.lossyScale.y / 2;
+		}
+		else
+		{ 
+			pos.y -= objHead.transform.lossyScale.y / 2;
+		}
+		
+		// 胴の生成
+		GameObject parentBody = new GameObject("Bodies");
+		parentBody.transform.parent = transform;
+
+		for(int i = 0; i < lengthBody; i++)
+		{
+
+			// 0回目のときだけ、自身の大きさの半分ずらす
+			if(i == 0)
+			{
+
+				pos.y -= prefabBody.transform.lossyScale.y / 2;
+				pos.y -= intervalBodytoBody;
+
+			}
+
+			GameObject o = Instantiate(prefabBody, (Vector3)pos, Quaternion.Euler(0f, 0f, angle), parentBody.transform);
+			pos.y -= o.transform.lossyScale.y + intervalBodytoBody;
+			pos.y -= intervalBodytoBody;
+
+		}
+
+	}
+
+}
